6da493c7a32b2efe0c3cbeba3d1fcf92975de04e
Jules Haezebrouck <jules.haezebrouck@ugent.be>
2026-06-02 15:06:16 +0200

    hide unused var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,11 +19,6 @@ variable "region" {
   default     = "europe-west1"
 }
 
-variable "domain_short" {
-  description = "Short domain identifier used in institute group naming"
-  type        = string
-}
-
 variable "app_domain" {
   description = "Public domain used by the HTTPS load balancer"
   type        = string
@@ -37,11 +32,6 @@ variable "ch_db_password" {
 
 variable "pg_db_password" {
   description = "Password for postgress"
-  type        = string
-}
-
-variable "image" {
-  description = "Full container image URL to deploy, e.g. europe-west1-docker.pkg.dev/<project>/<repo>/coga:<tag>"
   type        = string
 }
 
@@ -72,32 +62,32 @@ variable "azure_ad_client_secret_secret_id" {
 }
 
 
-variable "enable_cmek" {
-  description = "Create and manage a CMEK key for storage/build resources"
-  type        = bool
-  default     = true
-}
+# variable "enable_cmek" {
+#   description = "Create and manage a CMEK key for storage/build resources"
+#   type        = bool
+#   default     = true
+# }
 
-variable "cmek_key_ring_name" {
-  description = "KMS key ring name for CMEK"
-  type        = string
-  default     = "gen-kms-mgmt-euw1"
-}
+# variable "cmek_key_ring_name" {
+#   description = "KMS key ring name for CMEK"
+#   type        = string
+#   default     = "gen-kms-mgmt-euw1"
+# }
 
-variable "cmek_key_name" {
-  description = "KMS crypto key name for CMEK"
-  type        = string
-  default     = "gen-kms-mgmt-euw1"
-}
+# variable "cmek_key_name" {
+#   description = "KMS crypto key name for CMEK"
+#   type        = string
+#   default     = "gen-kms-mgmt-euw1"
+# }
 
-variable "cmek_rotation_period" {
-  description = "Rotation period for CMEK key"
-  type        = string
-  default     = "7776000s"
-}
+# variable "cmek_rotation_period" {
+#   description = "Rotation period for CMEK key"
+#   type        = string
+#   default     = "7776000s"
+# }
 
-variable "cmek_use_hsm" {
-  description = "Use HSM protection level for CMEK key"
-  type        = bool
-  default     = true
-}
+# variable "cmek_use_hsm" {
+#   description = "Use HSM protection level for CMEK key"
+#   type        = bool
+#   default     = true
+# }
